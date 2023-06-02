1efbbeddc18752235f148680c522acfd20c6b277
BuffManKyle <epsolfwd@gmail.com>
2023-06-02 16:58:51 -0400

    carf

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,6 +8,7 @@ import simpleillustration3 from './assets/images/simple-illustration-v3.png'
 import upctreelogo from './assets/images/UPC-tree-v2.png'
 import upccirclelogo from './assets/images/UPC-circletree-v1.png';
 import upcflowerlogo from './assets/images/UPC-flower-v1.png';
+import carf from './assets/images/CARF.png'
 
 function Home () {
     return (
@@ -18,18 +19,25 @@ function Home () {
         src={headerillustration}
         alt="Header img"
         />
+        <img 
+        src={carf}
+        alt="CARF"
+        className="carf-pos hide-on-mobile "
+        />
         <Container>
         <div className="">
-            <h1 className="hero-heading "> 
+            <h1 className="hero-heading"> 
                 Healing and Recovery: Integrated Treatment Solutions
             </h1>
         </div>
         </Container>
     </div>
     <Container>
-    <p className=""> 
+        <Row className="content-row">
+            <h4 className=""> 
                 Recovery Network and University Psychological Center offer integrated treatment for co-occurring and behavioral health disorders. Our team of professionals provides state-certified mental health and addiction services, inspiring inner healing. We focus on individualized care to help patients achieve personal goals and improve their quality of life. We ensure a safe and supportive transition during times of change. Thank you for considering us as trusted providers of comprehensive co-occurring behavioral health treatment.
-            </p>
+            </h4>
+        </Row>
     <Row>
         <Col>
         <img 
@@ -67,9 +75,11 @@ function Home () {
         </Container>
     </div>
         <Container>
-            <p>
+        <Row className="content-row">
+            <h4>
             “The mission of University Psychological Center, Inc. – dba, Recovery Network, is to foster a treatment environment that is both responsive and respectful to patients and families while providing a culturally competent continuum of evidence based substance abuse, mental health and case management practices, which expedite bio-psychosocial stabilization and re-entry into the community. The program’s goal of cultural awareness, or understanding the influences of patients social and cultural norms as it relates to behaviors and treatment, and cultural competence, as it relates to all persons served regardless of age, race, creed, sexual preference, gender identity, or socio-economic status.”
-​           </p>
+​           </h4>
+        </Row>
         </Container>
     <div className="hero" >
         <img 
@@ -86,31 +96,33 @@ function Home () {
         </Container>
     </div>
 
-    <Container >
-    <p>“University Psychological Center, Inc. – dba, Recovery Network’s vision is to promote change, growth, and wellness to individuals and families in the community.”
-​   </p>
-
-    <h1>Core Values</h1>
-    <ul>
-        <li>
-            Integrity- Ethical Care with Sound Construction
-        </li>
-        <li>
-            Diversity- Cultural Competencies with the Promotion of Inclusion and Diversity
-        </li>
-        <li>
-            Civility- Interactions of Staff and Patients are Professional, Respectful, and Courteous
-        </li>
-        <li>
-            Innovation- Blending Traditional and New-Age Methodologies for Integration of Care
-        </li>
-        <li>
-            Excellence- Constant Pursuit of Excellence
-        </li>
-    </ul>
-    <p>Coming Soon, CARF Accreditation process. Click here to learn more! 
-        Haga clic aquí para español
-    </p>
+    <Container>
+        <Row className="content-row">
+            <Col lg="6" md="auto" className="d-flex align-items-center justify-content-center">
+                <h4>“University Psychological Center, Inc. – dba, Recovery Network’s vision is to promote change, growth, and wellness to individuals and families in the community.”
+            ​   </h4>
+            </Col>
+            <Col lg="6" md="auto"  className="">
+                <h1>Core Values</h1>
+                <ul>
+                    <li>
+                        Integrity- Ethical Care with Sound Construction
+                    </li>
+                    <li>
+                        Diversity- Cultural Competencies with the Promotion of Inclusion and Diversity
+                    </li>
+                    <li>
+                        Civility- Interactions of Staff and Patients are Professional, Respectful, and Courteous
+                    </li>
+                    <li>
+                        Innovation- Blending Traditional and New-Age Methodologies for Integration of Care
+                    </li>
+                    <li>
+                        Excellence- Constant Pursuit of Excellence
+                    </li>
+                </ul>
+            </Col>
+    </Row>
     </Container>
      </>
     )
